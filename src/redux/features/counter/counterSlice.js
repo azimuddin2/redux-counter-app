cbde4cf1c04a05ef9277ccc9a6c4f0e4749309e9
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-03 23:59:10 +0600

    added redux toolkit

---
+++ src/redux/features/counter/counterSlice.js
@@ -0,0 +1,13 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+    count: 0,
+};
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {},
+});
+
+export default counterSlice.reducer;
